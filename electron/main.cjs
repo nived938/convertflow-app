23c8fe4f41a7ab5b9327f550168c1f8d28c13eca
powerplayexe <nivedsreejaharidasan@gmail.com>
2026-09-10 21:27:14 +0530

    Remove desktop Electron entrypoint

--- electron/main.cjs
+++
@@ -1,5 +0,0 @@
-const {app,BrowserWindow,shell}=require('electron');
-const path=require('path');
-function createWindow(){const win=new BrowserWindow({width:1280,height:850,minWidth:900,minHeight:650,webPreferences:{contextIsolation:true}});win.loadFile(path.join(__dirname,'../dist/index.html'));win.webContents.setWindowOpenHandler(({url})=>{shell.openExternal(url);return {action:'deny'}})}
-app.whenReady().then(()=>{createWindow();app.on('activate',()=>BrowserWindow.getAllWindows().length===0&&createWindow())});
-app.on('window-all-closed',()=>{if(process.platform!=='darwin')app.quit()});
